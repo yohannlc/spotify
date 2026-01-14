dae7fbea15561e898f0a1fac29d335e90aba2cb3
yohannlc <yohann.lecamx@gmail.com>
2026-01-14 20:38:22 +0100

    Fix erreur NG04002

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { WelcomeComponent } from './welcome/welcome.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: WelcomeComponent },
+  { path: 'callback', component: WelcomeComponent }
+];
